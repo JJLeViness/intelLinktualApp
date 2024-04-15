627296646d8cbfb93c9a8c1d11828491be96445e
JJLeViness <118782369+JJLeViness@users.noreply.github.com>
2024-04-14 20:45:43 -0400

    Calendar Activity will display dates, dates are selectable and will display the events for the day. Currently has dummmy events to show information. Pop Up window to add new events is in progress.

--- app/src/main/java/com/leviness/intellinktualapp/CalendarActivity.java
+++ app/src/main/java/com/leviness/intellinktualapp/CalendarActivity.java
@@ -1,11 +1,16 @@
 package com.leviness.intellinktualapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -17,11 +22,31 @@ public class CalendarActivity extends AppCompatActivity {
 
 
         ImageButton calendarHome = findViewById(R.id.calendar_home);
+        ImageButton addEvent=findViewById(R.id.addEvent_button);
         calendarHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent homeIntent = new Intent(CalendarActivity.this, HomeActivity.class);
                 startActivity(homeIntent);
+            }
+        });
+
+        addEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater inflater = LayoutInflater.from(CalendarActivity.this);
+                View popupView = inflater.inflate(R.layout.add_event_layout,null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(CalendarActivity.this);
+                builder.setView(popupView);
+
+                ImageButton finishAdd = findViewById(R.id.addEvent_imageButton);
+                EditText EventName = findViewById(R.id.enterEventName);
+                EditText eventDate = findViewById(R.id.editTextDate);
+                EditText eventTime = findViewById(R.id.editTextTime);
+
+                //builder.setPositiveButton(R.id.addEvent_button)
+
             }
         });
 
